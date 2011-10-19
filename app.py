3a251672b3e6f4993f6e24b51f5f2ce6faed214e
Mark Smith <mark@qq.is>
2011-10-18 19:43:24 -0700

    now we're starting to cook

--- app.py
+++ app.py
@@ -16,26 +16,34 @@ Written by Mark Smith <mark@qq.is>.
 
 import os
 import re
+import pprint
 from flask import Flask, render_template, redirect
 from markdown import markdown
 from time import time
+
 
 ARCHIVE = {}  # Yr -> Mo -> [ list of articles ]
 ARTICLE_CACHE_TIMER = None
 ARTICLES = {}  # slug => filename
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/article/<slug>")
+
+@app.route('/article/<slug>')
 def article(slug=None):
     global ARTICLES
     if slug is None:
         return redirect('/')
     if not slug in ARTICLES:
         return error_404()
+
+    with open(ARTICLES[slug], 'r') as f:
+        md = markdown(f.read())
+    return render_template('article.html', article=md)
 
 
 def error_404():
@@ -45,13 +53,15 @@ def error_404():
     return render_template('404.html')
 
 
+@app.before_request
 def refresh_article_list():
     '''Read through the filesystem and find articles.
 
     '''
     global ARTICLES, ARTICLE_CACHE_TIMER, ARCHIVE
-    if ARTICLE_CACHE_TIMER is None or time() > ARTICLE_CACHE_TIMER:
-        ARTICLE_CACHE_TIMER = time() + 10
+    if ARTICLE_CACHE_TIMER is not None and time() < ARTICLE_CACHE_TIMER:
+        return
+    ARTICLE_CACHE_TIMER = time() + 10
 
     if not os.path.isdir('articles'):
         return
@@ -74,7 +84,6 @@ def refresh_article_list():
         ARCHIVE[yr][mo].reverse()
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.before_request(refresh_article_list)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
