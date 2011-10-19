a4a417e1a62c246a5bd28fd37d2b45dfcf5dc535
Mark Smith <mark@qq.is>
2011-10-18 18:45:41 -0700

    round two

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
+'''aurora -- a simple, rest easy blog
+
+The idea for this software was taken directly from toto:
+
+    https://github.com/cloudhead/toto
+
+I loved the idea of a Git powered blog, but being the Python guy that I am
+it didn't seem right to be running something in Ruby.  Also, much harder
+and slower for me to work with.
+
+Thus: Aurora.
+
+Written by Mark Smith <mark@qq.is>.
+
+'''
+
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello from Python!"
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
