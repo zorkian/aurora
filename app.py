23f196f1d5e6713a14e7f527cb4bed1b5bd8dcfa
Mark Smith <mark@qq.is>
2011-10-18 19:16:37 -0700

    add some content now

--- app.py
+++ app.py
@@ -15,13 +15,66 @@ Written by Mark Smith <mark@qq.is>.
 '''
 
 import os
-from flask import Flask, render_template
+import re
+from flask import Flask, render_template, redirect
+from markdown import markdown
+from time import time
+
+ARCHIVE = {}  # Yr -> Mo -> [ list of articles ]
+ARTICLE_CACHE_TIMER = None
+ARTICLES = {}  # slug => filename
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+@app.route("/article/<slug>")
+def article(slug=None):
+    global ARTICLES
+    if slug is None:
+        return redirect('/')
+    if not slug in ARTICLES:
+        return error_404()
+
+
+def error_404():
+    '''Return a 404 page.
+
+    '''
+    return render_template('404.html')
+
+
+def refresh_article_list():
+    '''Read through the filesystem and find articles.
+
+    '''
+    global ARTICLES, ARTICLE_CACHE_TIMER, ARCHIVE
+    if ARTICLE_CACHE_TIMER is None or time() > ARTICLE_CACHE_TIMER:
+        ARTICLE_CACHE_TIMER = time() + 10
+
+    if not os.path.isdir('articles'):
+        return
+    for fn in os.listdir('articles'):
+        ffn = os.path.join('articles', fn)
+        if not os.path.isfile(ffn):
+            continue
+        match = re.match(r'^(\d\d\d\d)-(\d\d)-(\d\d)-(.+)\.md$', fn)
+        if match is None:
+            continue
+        yr, mo, da, slug = match.groups()
+        ARTICLES[slug] = ffn
+
+        if not yr in ARCHIVE:
+            ARCHIVE[yr] = {}
+        if not mo in ARCHIVE[yr]:
+            ARCHIVE[yr][mo] = []
+        ARCHIVE[yr][mo].append(ffn)
+        ARCHIVE[yr][mo].sort()
+        ARCHIVE[yr][mo].reverse()
+
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
+    app.before_request(refresh_article_list)
     app.run(host='0.0.0.0', port=port)
---
+++ articles/2011-10-18-just-a-test.md
@@ -0,0 +1,6 @@
+Testing
+=======
+
+This is a test.  This is only a test.  This should show up in the blog
+as a useful blog post.  Yet it's still human readable text if you look
+at the raw file.  Mm-hmm.
